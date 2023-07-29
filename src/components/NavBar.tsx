ff47654709f9e51a1949f4a22e82d127650dbe60
joshualeeee <102551497+joshualeeee@users.noreply.github.com>
2023-07-29 10:41:37 -1000

    feat: build color mode switch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,13 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import prem_logo from "../assets/prem_logo.webp";
 import React from "react";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent={"space-between"} padding="10px">
       <Image src={prem_logo} boxSize="60px"></Image>
-      <Text>NavBar</Text>
+      <ColorModeSwitch />
     </HStack>
   );
 };
